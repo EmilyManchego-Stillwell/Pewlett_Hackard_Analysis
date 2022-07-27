86c861feb6f989fd453f2f963ece3a47a06fbcfa
Emily Manchego-Stillwell <emanchego85@gmail.com>
2022-07-27 16:09:32 -0600

    created tables in PgAdmin and imported csv files related to each table. created a new table that has info related to employees that are ready to retire and qualify for the retirement package, that info was exported as a csv file.

--- Data/schema.sql
+++ Data/schema.sql
@@ -21,7 +21,7 @@ dept_no VARCHAR(4) NOT NULL,
 	to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	PRIMARY KEY(emp_no, dept_no)
+	PRIMARY KEY(emp_no)
 );
 CREATE TABLE salaries(
 	emp_no INT NOT NULL,
@@ -46,5 +46,5 @@ CREATE TABLE titles(
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no, title)
+	PRIMARY KEY (emp_no, title, from_date)
 );
